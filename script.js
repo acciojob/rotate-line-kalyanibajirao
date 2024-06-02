5ffac0cc761bf9350ab900ea584592f1c7c1f924
Kalyani Bajirao <95849305+kalyanibajirao@users.noreply.github.com>
2024-06-02 12:53:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const line = document.getElementById('line');
+        let angle = 0;
+
+        function rotateLine() {
+            angle += 2;
+            line.style.transform = `rotate(${angle}deg)`;
+            requestAnimationFrame(rotateLine);
+        }
+
+        // Start the rotation
+        rotateLine();
